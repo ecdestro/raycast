e64dd679d3a8659efa8f7661d3153de9f82ddc62
ecdestro <61299394+ecdestro@users.noreply.github.com>
2024-02-04 03:04:38 -0600

    Consolidated rc_engine script
    Run the rc_engine.py with a command line argument of a text file generated by rc_mapper.py to load that map.

---
+++ rc_engine.py
@@ -0,0 +1,224 @@
+import math
+from re import I
+import pygame as pg
+import sys
+
+args = sys.argv
+args_length = len(args)
+file_name = sys.argv[1]
+# game settings
+RES = WIDTH, HEIGHT = 1600, 900
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+FPS = 0
+
+PLAYER_POS = 1.5, 5 # mini_map
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
+SCALE = WIDTH // NUM_RAYS
+
+with open(file_name, 'r') as file:
+    mini_map = [list(map(int, line.split())) for line in file]
+
+class Map:
+    def __init__(self, game):
+        self.game = game
+        self.mini_map = mini_map
+        self.world_map = {}
+        self.get_map()
+
+    def get_map(self):
+        for j, row in enumerate(self.mini_map):
+            for i, value in enumerate(row):
+                if value:
+                    self.world_map[(i, j)] = value
+    
+    def draw(self):
+        [pg.draw.rect(self.game.screen, 'darkgray', (pos[0] * 100, pos[1] * 100, 100, 100), 2)
+        for pos in self.world_map]
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pg.K_d]:
+            dx += -speed_sin
+            dy += speed_cos
+
+        self.check_wall_collision(dx, dy)
+
+        if keys[pg.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+        
+    def draw(self):
+        # pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+        #             (self.x * 100 + WIDTH * math.cos(self.angle),
+        #              self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
+
+    def update(self):
+        self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
+    
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+
+    def ray_cast(self):
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+
+        ray_angle = self.game.player.angle - HALF_FOV + 0.0001
+        for ray in range(NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            # horizontals
+            y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)
+
+            depth_hor = (y_hor - oy) / sin_a
+            x_hor = ox + depth_hor * cos_a
+
+            delta_depth = dy / sin_a
+            dx = delta_depth * cos_a
+
+            for i in range(MAX_DEPTH):
+                tile_hor = int(x_hor), int(y_hor)
+                if tile_hor in self.game.map.world_map:
+                    break
+                x_hor += dx
+                y_hor += dy
+                depth_hor += delta_depth
+
+            # verticals
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+            depth_vert = (x_vert - ox) / cos_a
+            y_vert = oy + depth_vert * sin_a
+
+            delta_depth = dx / cos_a
+            dy = delta_depth * sin_a
+
+            for i in range(MAX_DEPTH):
+                tile_vert = int(x_vert), int(y_vert)
+                if tile_vert in self.game.map.world_map:
+                    break
+                x_vert += dx
+                y_vert += dy
+                depth_vert += delta_depth
+
+            # depth
+            if depth_vert < depth_hor:
+                depth = depth_vert
+            else:
+                depth = depth_hor
+
+            # remove fisheye effect
+            depth *= math.cos(self.game.player.angle - ray_angle)
+
+            # projection
+            proj_height = SCREEN_DIST / (depth + 0.0001)
+
+            # draw walls
+            color = [255 / (1 + depth ** 5 * 0.00002)] * 3
+            pg.draw.rect(self.game.screen, color,
+                         (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
+
+            # draw for debug -- also disable main.py lines 31, 32 
+            # pg.draw.line(self.game.screen, 'yellow', (100 * ox, 100 * oy),
+            #             (100 * ox + 100 * depth * cos_a, 100 * oy + 100 * depth * sin_a), 2)
+
+            ray_angle += DELTA_ANGLE
+
+    def update(self):
+        self.ray_cast()
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.new_game()
+
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+        self.raycasting = RayCasting(self)
+
+    def update(self):
+        self.player.update()
+        self.raycasting.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+    
+    def draw(self):
+        self.screen.fill('black')
+        # self.map.draw()
+        # self.player.draw()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
